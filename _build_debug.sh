11ae1cca45d107ebefdcbae072513b6ec956c733
zhaokai <zhaokai@yidian-inc.com>
2016-04-24 18:44:48 +0800

    Use gcc or g++ as compiler

--- _build_debug.sh
+++ _build_debug.sh
@@ -3,6 +3,6 @@
 mkdir -p build-cmake/Debug
 rm ./build-cmake/Debug/*unittest
 
-cmake -H. -B./build-cmake/Debug/obj -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_ASAN_TYPE=1
+cmake -H. -B./build-cmake/Debug/obj -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_ASAN_TYPE=1 -DCMAKE_C_COMPILER="/usr/bin/gcc" -DCMAKE_CXX_COMPILER="/usr/bin/g++"
 #-DCMAKE_CXX_COMPILER=/usr/local/clang+llvm-3.8.0-linux-x86_64-centos6/bin/clang++
 cmake --build ./build-cmake/Debug/obj -- -j4
