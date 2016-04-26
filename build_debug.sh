29522669a219e578fb5735294d6b66302d132285
zhaokai <zhaokai@yidian-inc.com>
2016-04-26 08:24:12 +0800

    Rename _build_debug.sh -> build_debug.sh

--- build_debug.sh
+++ build_debug.sh
@@ -6,3 +6,6 @@ rm ./build-cmake/Debug/*unittest
 cmake -H. -B./build-cmake/Debug/obj -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_ASAN_TYPE=1 -DCMAKE_C_COMPILER="/usr/bin/gcc" -DCMAKE_CXX_COMPILER="/usr/bin/g++"
 #-DCMAKE_CXX_COMPILER=/usr/local/clang+llvm-3.8.0-linux-x86_64-centos6/bin/clang++
 cmake --build ./build-cmake/Debug/obj -- -j4
+
+rm -f ./date_parser_unittest
+ln -s ./build-cmake/Debug/date_parser_unittest ./date_parser_unittest
